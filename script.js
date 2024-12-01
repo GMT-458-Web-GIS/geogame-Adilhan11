362bf306622d51f7de7ae1cb64626b7e487eece2
Adilhan11 <adlhan345@gmail.com>
2024-12-01 15:21:42 +0300

    division of territories in turkey

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const map = L.map('map').setView([39.9334, 32.8597], 6);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}@2x.png', {
@@ -5,90 +6,82 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}@2x.png
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
-const cities = [
-    { name: "Adana", lat: 37.0, lon: 35.3213 },
-    { name: "Adıyaman", lat: 37.7648, lon: 38.2786 },
-    { name: "Afyonkarahisar", lat: 38.7638, lon: 30.5403 },
-    { name: "Ağrı", lat: 39.7191, lon: 43.0503 },
-    { name: "Aksaray", lat: 38.3687, lon: 34.0362 },
-    { name: "Amasya", lat: 40.6499, lon: 35.8353 },
-    { name: "Ankara", lat: 39.9334, lon: 32.8597 },
-    { name: "Antalya", lat: 36.8969, lon: 30.7133 },
-    { name: "Ardahan", lat: 41.1105, lon: 42.7022 },
-    { name: "Artvin", lat: 41.1828, lon: 41.8183 },
-    { name: "Aydın", lat: 37.8444, lon: 27.8456 },
-    { name: "Balıkesir", lat: 39.6484, lon: 27.8826 },
-    { name: "Bartın", lat: 41.6354, lon: 32.339 },
-    { name: "Batman", lat: 37.8812, lon: 41.1351 },
-    { name: "Bayburt", lat: 40.2563, lon: 40.2249 },
-    { name: "Bilecik", lat: 40.1503, lon: 29.9807 },
-    { name: "Bingöl", lat: 38.8846, lon: 40.4939 },
-    { name: "Bitlis", lat: 38.4016, lon: 42.1078 },
-    { name: "Bolu", lat: 40.735, lon: 31.6066 },
-    { name: "Burdur", lat: 37.7269, lon: 30.2889 },
-    { name: "Bursa", lat: 40.1828, lon: 29.0668 },
-    { name: "Çanakkale", lat: 40.1467, lon: 26.4064 },
-    { name: "Çankırı", lat: 40.6013, lon: 33.6134 },
-    { name: "Çorum", lat: 40.5506, lon: 34.9556 },
-    { name: "Denizli", lat: 37.7833, lon: 29.0947 },
-    { name: "Diyarbakır", lat: 37.9136, lon: 40.2174 },
-    { name: "Düzce", lat: 40.8389, lon: 31.1639 },
-    { name: "Edirne", lat: 41.6772, lon: 26.5558 },
-    { name: "Elazığ", lat: 38.6809, lon: 39.2264 },
-    { name: "Erzincan", lat: 39.75, lon: 39.4926 },
-    { name: "Erzurum", lat: 39.9043, lon: 41.2679 },
-    { name: "Eskişehir", lat: 39.7667, lon: 30.5256 },
-    { name: "Gaziantep", lat: 37.0662, lon: 37.3833 },
-    { name: "Giresun", lat: 40.9128, lon: 38.3895 },
-    { name: "Gümüşhane", lat: 40.4602, lon: 39.4673 },
-    { name: "Hakkari", lat: 37.5744, lon: 43.7408 },
-    { name: "Hatay", lat: 36.2, lon: 36.1603 },
-    { name: "Iğdır", lat: 39.9237, lon: 44.045 },
-    { name: "Isparta", lat: 37.7648, lon: 30.5561 },
-    { name: "İstanbul", lat: 41.0082, lon: 28.9784 },
-    { name: "İzmir", lat: 38.4237, lon: 27.1428 },
-    { name: "Kahramanmaraş", lat: 37.5753, lon: 36.9371 },
-    { name: "Karabük", lat: 41.204, lon: 32.625 },
-    { name: "Karaman", lat: 37.1814, lon: 33.215 },
-    { name: "Kars", lat: 40.6016, lon: 43.0944 },
-    { name: "Kastamonu", lat: 41.3894, lon: 33.7831 },
-    { name: "Kayseri", lat: 38.7333, lon: 35.491 },
-    { name: "Kırıkkale", lat: 39.8468, lon: 33.5155 },
-    { name: "Kırklareli", lat: 41.7351, lon: 27.2253 },
-    { name: "Kırşehir", lat: 39.1458, lon:34.1638 },
-    { name: "Kilis", lat: 36.7184, lon: 37.1212 },
-    { name: "Kocaeli", lat: 40.8533, lon: 29.8815 },
-    { name: "Konya", lat: 37.8667, lon: 32.4833 },
-    { name: "Kütahya", lat: 39.4242, lon: 29.9833 },
-    { name: "Malatya", lat: 38.3552, lon: 38.3095 },
-    { name: "Manisa", lat: 38.6191, lon: 27.4289 },
-    { name: "Mardin", lat: 37.3131, lon: 40.7436 },
-    { name: "Mersin", lat: 36.8121, lon: 34.6415 },
-    { name: "Muğla", lat: 37.2153, lon: 28.3636 },
-    { name: "Muş", lat: 38.9462, lon: 41.7539 },
-    { name: "Nevşehir", lat: 38.625, lon: 34.7122 },
-    { name: "Niğde", lat: 37.9667, lon: 34.6833 },
-    { name: "Ordu", lat: 40.9862, lon: 37.8797 },
-    { name: "Osmaniye", lat: 37.0742, lon: 36.2474 },
-    { name: "Rize", lat: 41.0201, lon: 40.5234 },
-    { name: "Sakarya", lat: 40.7731, lon: 30.3948 },
-    { name: "Samsun", lat: 41.2867, lon: 36.33 },
-    { name: "Siirt", lat: 37.9333, lon: 41.95 },
-    { name: "Sinop", lat: 42.0264, lon: 35.1551 },
-    { name: "Sivas", lat: 39.7477, lon: 37.0179 },
-    { name: "Şanlıurfa", lat: 37.1671, lon: 38.7939 },
-    { name: "Şırnak", lat: 37.4187, lon: 42.4918 },
-    { name: "Tekirdağ", lat: 40.978, lon: 27.5117 },
-    { name: "Tokat", lat: 40.3133, lon: 36.5497 },
-    { name: "Trabzon", lat: 41.0053, lon: 39.7338 },
-    { name: "Tunceli", lat: 39.0667, lon: 39.1783 },
-    { name: "Uşak", lat: 38.6781, lon: 29.4083 },
-    { name: "Van", lat: 38.4237, lon: 43.3665 },
-    { name: "Yalova", lat: 40.6333, lon: 29.2667 },
-    { name: "Yozgat", lat: 39.8208, lon: 34.8125 },
-    { name: "Zonguldak", lat: 41.4543, lon: 31.7987 },
-];
+// Cities categorized by regions
+const regions = {
+    akdeniz: [
+        { name: "Adana", lat: 37.0, lon: 35.3213 },
+        { name: "Antalya", lat: 36.8949, lon: 30.7133 },
+        { name: "Hatay", lat: 36.2, lon: 36.1667 },
+        { name: "Isparta", lat: 37.7667, lon: 30.55 },
+        { name: "Kahramanmaraş", lat: 37.5858, lon: 36.9371 },
+        { name: "Mersin", lat: 36.8121, lon: 34.6415 },
+        { name: "Osmaniye", lat: 37.0742, lon: 36.2474 },
+        { name: "Burdur", lat: 37.7203, lon: 30.2908 }
+    ],
+    doguAnadolu: [
+        { name: "Ağrı", lat: 39.7191, lon: 43.0503 },
+        { name: "Erzurum", lat: 39.9, lon: 41.2769 },
+        { name: "Kars", lat: 40.6074, lon: 43.0979 },
+        { name: "Malatya", lat: 38.3552, lon: 38.3095 },
+        { name: "Van", lat: 38.4946, lon: 43.3832 },
+        { name: "Tunceli", lat: 39.1069, lon: 39.5481 },
+        { name: "Elazığ", lat: 38.681, lon: 39.2264 },
+        { name: "Bingöl", lat: 38.8845, lon: 40.4965 }
+    ],
+    ege: [
+        { name: "Afyonkarahisar", lat: 38.7638, lon: 30.5405 },
+        { name: "İzmir", lat: 38.4237, lon: 27.1428 },
+        { name: "Manisa", lat: 38.6191, lon: 27.4289 },
+        { name: "Muğla", lat: 37.2153, lon: 28.3636 },
+        { name: "Denizli", lat: 37.7765, lon: 29.0864 },
+        { name: "Aydın", lat: 37.8444, lon: 27.8458 },
+        { name: "Kütahya", lat: 39.4242, lon: 29.9833 },
+        { name: "Uşak", lat: 38.6833, lon: 29.4167 }
+    ],
+    guneydoguAnadolu: [
+        { name: "Adıyaman", lat: 37.7648, lon: 38.276 },
+        { name: "Diyarbakır", lat: 37.9158, lon: 40.2189 },
+        { name: "Gaziantep", lat: 37.0662, lon: 37.3833 },
+        { name: "Mardin", lat: 37.3132, lon: 40.735 },
+        { name: "Siirt", lat: 37.9346, lon: 41.9463 },
+        { name: "Şanlıurfa", lat: 37.1674, lon: 38.7955 },
+        { name: "Batman", lat: 37.8812, lon: 41.1321 },
+        { name: "Kilis", lat: 36.7167, lon: 37.1167 }
+    ],
+    icAnadolu: [
+        { name: "Ankara", lat: 39.9334, lon: 32.8597 },
+        { name: "Eskişehir", lat: 39.7667, lon: 30.5256 },
+        { name: "Konya", lat: 37.8667, lon: 32.4833 },
+        { name: "Nevşehir", lat: 38.625, lon: 34.7122 },
+        { name: "Kayseri", lat: 38.7333, lon: 35.491 },
+        { name: "Aksaray", lat: 38.3687, lon: 34.0362 },
+        { name: "Niğde", lat: 37.9667, lon: 34.6833 }
+    ],
+    karadeniz: [
+        { name: "Artvin", lat: 41.1831, lon: 41.8203 },
+        { name: "Samsun", lat: 41.2797, lon: 36.3361 },
+        { name: "Trabzon", lat: 41.0015, lon: 39.7178 },
+        { name: "Rize", lat: 41.0201, lon: 40.5234 },
+        { name: "Ordu", lat: 40.9847, lon: 37.8785 },
+        { name: "Giresun", lat: 40.918, lon: 38.3895 },
+        { name: "Zonguldak", lat: 41.4564, lon: 31.7987 }
+    ],
+    marmara: [
+        { name: "İstanbul", lat: 41.0082, lon: 28.9784 },
+        { name: "Edirne", lat: 41.6772, lon: 26.555 },
+        { name: "Tekirdağ", lat: 40.978, lon: 27.511 },
+        { name: "Kocaeli", lat: 40.7667, lon: 29.9167 },
+        { name: "Sakarya", lat: 40.6936, lon: 30.4358 },
+        { name: "Bursa", lat: 40.1826, lon: 29.0664 },
+        { name: "Balıkesir", lat: 39.6484, lon: 27.8826 }
+    ]
+};
 
+
+let regionOrder = Object.keys(regions); // Bölge sıralaması
+let currentRegionIndex = 0;
+let currentRegion = regionOrder[currentRegionIndex];
+let availableCities = [...regions[currentRegion]];
 let currentCityIndex = 0;
 let marker;
 let score = 0;
@@ -116,7 +109,7 @@ function addGreenMarker(city) {
         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
         shadowSize: [41, 41]
     });
-    
+
     const greenMarker = L.marker([city.lat, city.lon], { icon: greenIcon }).addTo(map);
     greenMarkers.push(greenMarker); 
 }
@@ -130,11 +123,10 @@ function placeMarker(city) {
 }
 
 function startGame() {
-    currentCityIndex = Math.floor(Math.random() * cities.length);
-    placeMarker(cities[currentCityIndex]);
+    currentCityIndex = Math.floor(Math.random() * availableCities.length);
+    placeMarker(availableCities[currentCityIndex]);
     cityInput.value = "";
     messageDisplay.textContent = "";
-    
 }
 
 function updateScore() {
@@ -156,11 +148,28 @@ function updateTimer() {
     }
 }
 
+function checkIfRegionCompleted() {
+    if (availableCities.length === 0) { // Tüm şehirler doğru bilindiğinde
+        messageDisplay.textContent = `Congratulations! You've completed the ${currentRegion} region! Another minute will be added!`;
+        timeLeft += 60; // 1 dakika ekleniyor
+        currentRegionIndex++; // Bir sonraki bölgeye geç
+        if (currentRegionIndex < regionOrder.length) {
+            currentRegion = regionOrder[currentRegionIndex];
+            availableCities = [...regions[currentRegion]]; // Yeni bölgenin şehirlerini yükle
+            startGame(); // Yeni bölge için oyunu başlat
+        } else {
+            // Eğer tüm bölgeler tamamlanmışsa oyunu bitir
+            messageDisplay.textContent = "Congratulations! You've completed all regions! You won the game!";
+            endGame(); // Oyunu bitir
+        }
+    }
+}
+
+
 function endGame() {
     totalScore = score - (skipped * 2);
     clearInterval(timer);
     messageDisplay.textContent = `Time's up! Your total score is: ${totalScore}`;
-
     greenMarkers.forEach(marker => {
         map.removeLayer(marker);
     });
@@ -188,28 +197,37 @@ function wrongGuess() {
 
 submitBtn.addEventListener("click", function () {
     const guess = cityInput.value.trim().toLowerCase();
-    const currentCity = cities[currentCityIndex].name.toLowerCase();
+    const currentCity = availableCities[currentCityIndex].name.toLowerCase();
 
     if (guess === currentCity) {
         updateScore();
-        addGreenMarker(cities[currentCityIndex]); 
-        correctGuess(); 
-        currentCityIndex = Math.floor(Math.random() * cities.length);
-        placeMarker(cities[currentCityIndex]);
-        cityInput.value = "";
-        messageDisplay.textContent = "Correct! Keep going!";
+        addGreenMarker(availableCities[currentCityIndex]);
+        correctGuess();
+        availableCities.splice(currentCityIndex, 1); // Doğru tahmin edilen şehri kaldır
+        if (availableCities.length > 0) {
+            currentCityIndex = Math.floor(Math.random() * availableCities.length);
+            placeMarker(availableCities[currentCityIndex]);
+            cityInput.value = "";
+            messageDisplay.textContent = "Correct! Keep going!";
+        }
+        checkIfRegionCompleted(); // Doğru tahminden sonra bölge kontrolü yap
     } else {
-        wrongGuess(); 
+        wrongGuess();
         messageDisplay.textContent = "Wrong! Try again.";
     }
 });
 
 passBtn.addEventListener("click", function () {
     updateSkipped();
-    currentCityIndex = Math.floor(Math.random() * cities.length);
-    placeMarker(cities[currentCityIndex]);
-    cityInput.value = "";
-    messageDisplay.textContent = "City skipped!";
+    availableCities.splice(currentCityIndex, 1); // remove skipped city
+    if (availableCities.length > 0) {
+        currentCityIndex = Math.floor(Math.random() * availableCities.length);
+        placeMarker(availableCities[currentCityIndex]);
+        cityInput.value = "";
+        messageDisplay.textContent = "City skipped!";
+    } else {
+        endGame(); // No available cities left
+    }
 });
 
 restartBtn.addEventListener("click", function () {
@@ -223,8 +241,10 @@ restartBtn.addEventListener("click", function () {
     cityInput.disabled = false;
     submitBtn.disabled = false;
     passBtn.disabled = false;
-    startGame();
-    timer = setInterval(updateTimer, 1000);
+    currentRegion = "icAnadolu"; // Başlangıç bölgesi "İç Anadolu"
+    availableCities = [...regions[currentRegion]]; // Bölgedeki şehirleri yükle
+    startGame(); // Oyunu başlat
+    timer = setInterval(updateTimer, 1000); // Zamanlayıcıyı başlat
 });
 
 // Rules modal functionality
@@ -246,15 +266,16 @@ window.addEventListener("click", (event) => {
     }
 });
 
-// Start Game button functionality
-const startBtn = document.getElementById("start-btn");
 
+const startBtn = document.getElementById("start-btn");
 startBtn.addEventListener("click", function () {
-    startBtn.style.display = "none"; // Hide Start Game button after click
-    restartBtn.style.display = "none"; // Ensure Restart button is hidden at start
+    startBtn.style.display = "none"; 
+    restartBtn.style.display = "none"; 
     cityInput.disabled = false;
     submitBtn.disabled = false;
     passBtn.disabled = false;
-    startGame(); // Start the game
-    timer = setInterval(updateTimer, 1000); // Start timer
+    currentRegion = "icAnadolu"; 
+    availableCities = [...regions[currentRegion]]; 
+    startGame(); 
+    timer = setInterval(updateTimer, 1000); 
 });
